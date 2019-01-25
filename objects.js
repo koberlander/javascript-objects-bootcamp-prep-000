1f6dd0c5c4459f900e0b69a8c614602b80042f70
ko kristin <koberlander115@gmail.com>
2019-01-25 21:52:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.slowdive;
+  delete playlist[artistName];
   return playlist;
 }
